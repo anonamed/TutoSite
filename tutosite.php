816467e78c98948c920a018e8845c1ad2122d5d7
Hugo Flamant <hugo.flamant@eemi.com>
2016-10-18 18:44:23 +0200

    seconds commit

--- tutosite.php
+++ tutosite.php
@@ -4,13 +4,13 @@ Plugin Name: TutoSite
 Description: Un plugin servant à créer un tutoriel d'utilisation
 Version: 0.1
 Author: Hugo Ft
-License: GPL2
+License: GPL3
 */
 
 add_action( 'admin_menu', 'my_plugin_menu' );
 
 function my_plugin_menu() {
-    add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+    add_options_page( 'TutoSite Options', 'TutoSite', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
 }
 
 function my_plugin_options() {
@@ -20,4 +20,20 @@ function my_plugin_options() {
     echo '<div class="wrap">';
     echo '<p>Here is where you can modified the option for create the tutorial :</p>';
     echo '</div>';
+}
+
+function modified_balise() {
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js', null, '3.1.0', 'true');
+    add_action('wp_footer', 'modified_balise_script');
+}
+
+function modified_balise_script() {
+    ?>
+    <script type="text/javascript">
+        (function($){
+            $('modified_balise')
+        })(jQuery);
+    </script>
+<?php
 }
